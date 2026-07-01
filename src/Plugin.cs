69a85e6fc5072ab31ee0c230d37c9dfdfbad5e0e
Benny Dreamly <bgaming0277@gmail.com>
2026-06-30 20:19:14 -0600

    change all of this to be just for the freeloadout stuff

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -12,11 +12,11 @@ using static PowerwashSimAP.Locations;
 
 namespace PowerwashSimAP;
 
-[BepInPlugin("PowerwashSimAP", "PowerwashSimArchipelagoRandomizer", VersionNumber)]
+[BepInPlugin("FreeLoadout", "FreeLoadout", VersionNumber)]
 public class Plugin : BasePlugin
 {
-    public const string VersionNumber = "v0.5.14";
-    public const string ModDir = "BepInEx/plugins/SW_CreeperKing.ArchipelagoMod";
+    public const string VersionNumber = "v1.0.0";
+    public const string ModDir = "BepInEx/plugins/FreeLoadout";
 
     public enum DebugWant
     {
@@ -36,113 +36,113 @@ public class Plugin : BasePlugin
 
     public override void Load()
     {
-        if (File.Exists("debug.txt"))
-        {
-            if (int.TryParse(File.ReadAllText("debug.txt"), out var isDebug))
-            {
-                IsDebug = (DebugWant)isDebug;
-            }
-        }
+        // if (File.Exists("debug.txt"))
+        // {
+        //     if (int.TryParse(File.ReadAllText("debug.txt"), out var isDebug))
+        //     {
+        //         IsDebug = (DebugWant)isDebug;
+        //     }
+        // }
+        //
+        // Log = base.Log;
+        // Log.LogInfo($"Debug Setting: [{IsDebug}]");
+        //
+        // if (!File.Exists($"{ModDir}/Locations.txt"))
+        // {
+        //     Log.LogError("NO LOCATIONS FOUND, EXITING LOAD");
+        //     return;
+        // }
+        //
+        // if (!File.Exists($"{ModDir}/LocationTransformData.txt"))
+        // {
+        //     Log.LogError("NO LOCATION TRANSFORMS FOUND, EXITING LOAD");
+        //     return;
+        // }
+        //
+        // RawLocationData = File.ReadAllText($"{ModDir}/LocationTransformData.txt")
+        //                       .Replace("\r", "")
+        //                       .Split('\n')
+        //                       .Where(line => !line.StartsWith("//") && line != "")
+        //                       .Select(line => line.Split(','))
+        //                       .ToArray();
+        //
+        // CleanParts = File.ReadAllText($"{ModDir}/Locations.txt")
+        //                  .Replace("\r", "")
+        //                  .Split('\n')
+        //                  .Select(line => line.Split(':'))
+        //                  .ToArray()
+        //                  .ToDictionary(arr => arr[0], arr => arr[1].Split(','));
+        //
+        // LevelUnlockDictionary = RawLocationData.ToDictionary(arr => $"{arr[1]} Unlock", arr => arr[0]);
+        // LevelDictionary = RawLocationData.ToDictionary(arr => arr[1], arr => arr[0]);
+        // SceneNameToLocationName = RawLocationData.ToDictionary(arr => arr[2], arr => arr[1]);
+        // LabelNameToLocationName = RawLocationData.ToDictionary(arr => arr[0], arr => arr[1]);
+        //
+        // MainMenuButtonPatch.Lookahead = new()
+        // {
+        //     // base game
+        //     ["MainMenuButton_CareerOverview"] = ["CareerOverviewScreen(Clone)", "MainMenuButton_Specials"],
+        //     ["CareerOverviewScreen(Clone)"] = ["LocationsButton", "VehiclesButton"],
+        //     ["VehiclesButton"] = RawLocationData.Take(18).Select(arr => arr[0]).ToArray(),
+        //     ["FilterButton(Clone) (0)"] = RawLocationData.Take(12).Select(arr => arr[0]).ToArray(),
+        //     ["FilterButton(Clone) (1)"] = RawLocationData.Skip(12).Take(2).Select(arr => arr[0]).ToArray(),
+        //     ["FilterButton(Clone) (2)"] = RawLocationData.Skip(14).Take(4).Select(arr => arr[0]).ToArray(),
+        //     ["LocationsButton"] = RawLocationData.Skip(18).Take(20).Select(arr => arr[0]).ToArray(),
+        //
+        //     // bonus jobs
+        //     ["MainMenuButton_Specials"] =
+        //     [
+        //         "BonusGridElement(Clone) (0)", "BonusGridElement(Clone) (1)", "BonusGridElement(Clone) (2)",
+        //         "BonusGridElement(Clone) (3)", "BonusGridElement(Clone) (4)", "BonusGridElement(Clone) (5)",
+        //         "BonusGridElement(Clone) (6)"
+        //     ],
+        //     ["BonusGridElement(Clone) (0)"] = RawLocationData.Skip(38).Take(4).Select(arr => arr[0]).ToArray(),
+        //     ["BonusGridElement(Clone) (1)"] = RawLocationData.Skip(42).Take(3).Select(arr => arr[0]).ToArray(),
+        //     ["BonusGridElement(Clone) (2)"] = RawLocationData.Skip(45).Take(3).Select(arr => arr[0]).ToArray(),
+        //     ["BonusGridElement(Clone) (3)"] = RawLocationData.Skip(48).Take(2).Select(arr => arr[0]).ToArray(),
+        //     ["BonusGridElement(Clone) (4)"] = RawLocationData.Skip(50).Take(2).Select(arr => arr[0]).ToArray(),
+        //     ["BonusGridElement(Clone) (5)"] = RawLocationData.Skip(52).Take(1).Select(arr => arr[0]).ToArray(),
+        //     ["BonusGridElement(Clone) (6)"] = RawLocationData.Skip(53).Take(2).Select(arr => arr[0]).ToArray(),
+        //
+        //     // dlc
+        //     ["MainMenuButton_DLC"] =
+        //     [
+        //         "DLCGridElement(Clone) (0)", "DLCGridElement(Clone) (1)", "DLCGridElement(Clone) (2)",
+        //         "DLCGridElement(Clone) (3)", "DLCGridElement(Clone) (4)", "DLCGridElement(Clone) (5)",
+        //         "DLCGridElement(Clone) (6)", "DLCGridElement(Clone) (7)"
+        //     ],
+        //     ["DLCGridElement(Clone) (6)"] = ["FinalFantasyCampaignScreen(Clone)"],
+        //     ["FinalFantasyCampaignScreen(Clone)"] = RawLocationData.Skip(55).Take(5).Select(arr => arr[0]).ToArray(),
+        //     ["DLCGridElement(Clone) (7)"] = ["TombRaiderCampaignScreen(Clone)"],
+        //     ["TombRaiderCampaignScreen(Clone)"] = RawLocationData.Skip(60).Take(5).Select(arr => arr[0]).ToArray(),
+        //
+        //     // paid dlc
+        //     ["DLCGridElement(Clone) (0)"] = ["CheeseCampaignScreen(Clone)"],
+        //     ["CheeseCampaignScreen(Clone)"] = RawLocationData.Skip(65).Take(5).Select(arr => arr[0]).ToArray(),
+        //     ["DLCGridElement(Clone) (1)"] = ["ShrekCampaignScreen(Clone)"],
+        //     ["ShrekCampaignScreen(Clone)"] = RawLocationData.Skip(70).Take(5).Select(arr => arr[0]).ToArray(),
+        //     ["DLCGridElement(Clone) (2)"] = ["AAPCampaignScreen(Clone)"],
+        //     ["AAPCampaignScreen(Clone)"] = RawLocationData.Skip(75).Take(5).Select(arr => arr[0]).ToArray(),
+        //     ["DLCGridElement(Clone) (3)"] = ["Warhammer40KCampaignScreen(Clone)"],
+        //     ["Warhammer40KCampaignScreen(Clone)"] = RawLocationData.Skip(80).Take(5).Select(arr => arr[0]).ToArray(),
+        //     ["DLCGridElement(Clone) (4)"] = ["BTTFCampaignScreen(Clone)"],
+        //     ["BTTFCampaignScreen(Clone)"] = RawLocationData.Skip(85).Take(5).Select(arr => arr[0]).ToArray(),
+        //     ["DLCGridElement(Clone) (5)"] = ["SpongeBobCampaignScreen(Clone)"],
+        //     ["SpongeBobCampaignScreen(Clone)"] = RawLocationData.Skip(90).Take(6).Select(arr => arr[0]).ToArray(),
+        // };
 
-        Log = base.Log;
-        Log.LogInfo($"Debug Setting: [{IsDebug}]");
-
-        if (!File.Exists($"{ModDir}/Locations.txt"))
-        {
-            Log.LogError("NO LOCATIONS FOUND, EXITING LOAD");
-            return;
-        }
-
-        if (!File.Exists($"{ModDir}/LocationTransformData.txt"))
-        {
-            Log.LogError("NO LOCATION TRANSFORMS FOUND, EXITING LOAD");
-            return;
-        }
-
-        RawLocationData = File.ReadAllText($"{ModDir}/LocationTransformData.txt")
-                              .Replace("\r", "")
-                              .Split('\n')
-                              .Where(line => !line.StartsWith("//") && line != "")
-                              .Select(line => line.Split(','))
-                              .ToArray();
-
-        CleanParts = File.ReadAllText($"{ModDir}/Locations.txt")
-                         .Replace("\r", "")
-                         .Split('\n')
-                         .Select(line => line.Split(':'))
-                         .ToArray()
-                         .ToDictionary(arr => arr[0], arr => arr[1].Split(','));
-
-        LevelUnlockDictionary = RawLocationData.ToDictionary(arr => $"{arr[1]} Unlock", arr => arr[0]);
-        LevelDictionary = RawLocationData.ToDictionary(arr => arr[1], arr => arr[0]);
-        SceneNameToLocationName = RawLocationData.ToDictionary(arr => arr[2], arr => arr[1]);
-        LabelNameToLocationName = RawLocationData.ToDictionary(arr => arr[0], arr => arr[1]);
-
-        MainMenuButtonPatch.Lookahead = new()
-        {
-            // base game
-            ["MainMenuButton_CareerOverview"] = ["CareerOverviewScreen(Clone)", "MainMenuButton_Specials"],
-            ["CareerOverviewScreen(Clone)"] = ["LocationsButton", "VehiclesButton"],
-            ["VehiclesButton"] = RawLocationData.Take(18).Select(arr => arr[0]).ToArray(),
-            ["FilterButton(Clone) (0)"] = RawLocationData.Take(12).Select(arr => arr[0]).ToArray(),
-            ["FilterButton(Clone) (1)"] = RawLocationData.Skip(12).Take(2).Select(arr => arr[0]).ToArray(),
-            ["FilterButton(Clone) (2)"] = RawLocationData.Skip(14).Take(4).Select(arr => arr[0]).ToArray(),
-            ["LocationsButton"] = RawLocationData.Skip(18).Take(20).Select(arr => arr[0]).ToArray(),
-
-            // bonus jobs
-            ["MainMenuButton_Specials"] =
-            [
-                "BonusGridElement(Clone) (0)", "BonusGridElement(Clone) (1)", "BonusGridElement(Clone) (2)",
-                "BonusGridElement(Clone) (3)", "BonusGridElement(Clone) (4)", "BonusGridElement(Clone) (5)",
-                "BonusGridElement(Clone) (6)"
-            ],
-            ["BonusGridElement(Clone) (0)"] = RawLocationData.Skip(38).Take(4).Select(arr => arr[0]).ToArray(),
-            ["BonusGridElement(Clone) (1)"] = RawLocationData.Skip(42).Take(3).Select(arr => arr[0]).ToArray(),
-            ["BonusGridElement(Clone) (2)"] = RawLocationData.Skip(45).Take(3).Select(arr => arr[0]).ToArray(),
-            ["BonusGridElement(Clone) (3)"] = RawLocationData.Skip(48).Take(2).Select(arr => arr[0]).ToArray(),
-            ["BonusGridElement(Clone) (4)"] = RawLocationData.Skip(50).Take(2).Select(arr => arr[0]).ToArray(),
-            ["BonusGridElement(Clone) (5)"] = RawLocationData.Skip(52).Take(1).Select(arr => arr[0]).ToArray(),
-            ["BonusGridElement(Clone) (6)"] = RawLocationData.Skip(53).Take(2).Select(arr => arr[0]).ToArray(),
-
-            // dlc
-            ["MainMenuButton_DLC"] =
-            [
-                "DLCGridElement(Clone) (0)", "DLCGridElement(Clone) (1)", "DLCGridElement(Clone) (2)",
-                "DLCGridElement(Clone) (3)", "DLCGridElement(Clone) (4)", "DLCGridElement(Clone) (5)",
-                "DLCGridElement(Clone) (6)", "DLCGridElement(Clone) (7)"
-            ],
-            ["DLCGridElement(Clone) (6)"] = ["FinalFantasyCampaignScreen(Clone)"],
-            ["FinalFantasyCampaignScreen(Clone)"] = RawLocationData.Skip(55).Take(5).Select(arr => arr[0]).ToArray(),
-            ["DLCGridElement(Clone) (7)"] = ["TombRaiderCampaignScreen(Clone)"],
-            ["TombRaiderCampaignScreen(Clone)"] = RawLocationData.Skip(60).Take(5).Select(arr => arr[0]).ToArray(),
-
-            // paid dlc
-            ["DLCGridElement(Clone) (0)"] = ["CheeseCampaignScreen(Clone)"],
-            ["CheeseCampaignScreen(Clone)"] = RawLocationData.Skip(65).Take(5).Select(arr => arr[0]).ToArray(),
-            ["DLCGridElement(Clone) (1)"] = ["ShrekCampaignScreen(Clone)"],
-            ["ShrekCampaignScreen(Clone)"] = RawLocationData.Skip(70).Take(5).Select(arr => arr[0]).ToArray(),
-            ["DLCGridElement(Clone) (2)"] = ["AAPCampaignScreen(Clone)"],
-            ["AAPCampaignScreen(Clone)"] = RawLocationData.Skip(75).Take(5).Select(arr => arr[0]).ToArray(),
-            ["DLCGridElement(Clone) (3)"] = ["Warhammer40KCampaignScreen(Clone)"],
-            ["Warhammer40KCampaignScreen(Clone)"] = RawLocationData.Skip(80).Take(5).Select(arr => arr[0]).ToArray(),
-            ["DLCGridElement(Clone) (4)"] = ["BTTFCampaignScreen(Clone)"],
-            ["BTTFCampaignScreen(Clone)"] = RawLocationData.Skip(85).Take(5).Select(arr => arr[0]).ToArray(),
-            ["DLCGridElement(Clone) (5)"] = ["SpongeBobCampaignScreen(Clone)"],
-            ["SpongeBobCampaignScreen(Clone)"] = RawLocationData.Skip(90).Take(6).Select(arr => arr[0]).ToArray(),
-        };
-
-        ClassInjector.RegisterTypeInIl2Cpp<APGui>();
-        ClassInjector.RegisterTypeInIl2Cpp<MainMenuButtonPatch.AlwaysInvisible>();
-        ClassInjector.RegisterTypeInIl2Cpp<MainMenuButtonPatch.VisibleControlComponent>();
-        ClassInjector.RegisterTypeInIl2Cpp<WashTargetPatch.WashTargetUpdate>();
-        ClassInjector.RegisterTypeInIl2Cpp<Updatinator>();
-        Harmony.CreateAndPatchAll(typeof(LevelProgressionPatch));
-        Harmony.CreateAndPatchAll(typeof(JobLevelPatch));
-        Harmony.CreateAndPatchAll(typeof(MainMenuPatch));
-        Harmony.CreateAndPatchAll(typeof(MainMenuButtonPatch));
-        Harmony.CreateAndPatchAll(typeof(WashTargetPatch));
+        // ClassInjector.RegisterTypeInIl2Cpp<APGui>();
+        // ClassInjector.RegisterTypeInIl2Cpp<MainMenuButtonPatch.AlwaysInvisible>();
+        // ClassInjector.RegisterTypeInIl2Cpp<MainMenuButtonPatch.VisibleControlComponent>();
+        // ClassInjector.RegisterTypeInIl2Cpp<WashTargetPatch.WashTargetUpdate>();
+        // ClassInjector.RegisterTypeInIl2Cpp<Updatinator>();
+        // Harmony.CreateAndPatchAll(typeof(LevelProgressionPatch));
+        // Harmony.CreateAndPatchAll(typeof(JobLevelPatch));
+        // Harmony.CreateAndPatchAll(typeof(MainMenuPatch));
+        // Harmony.CreateAndPatchAll(typeof(MainMenuButtonPatch));
+        // Harmony.CreateAndPatchAll(typeof(WashTargetPatch));
         // Harmony.CreateAndPatchAll(typeof(HasJobBeenPlayedPatch));
-        Harmony.CreateAndPatchAll(typeof(FreePlayUnlockPatch));
+        // Harmony.CreateAndPatchAll(typeof(FreePlayUnlockPatch));
         // Harmony.CreateAndPatchAll(typeof(ShopPatch));
         // Harmony.CreateAndPatchAll(typeof(BuyPowerWasherPatch));
         Harmony.CreateAndPatchAll(typeof(UnlockAllEquipmentPatch));
